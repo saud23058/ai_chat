a5529b6cc92d968e5273814d2f5a928cdd111593
Saud_Khan <asaud3508@gmail.com>
2025-02-24 23:51:31 +0500

    url changed

--- lib/redux/features/chatApiSlice.ts
+++ lib/redux/features/chatApiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const chatApiSlice = createApi({
   reducerPath: "aiApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000", }),
   endpoints: (builder) => ({
     prompt: builder.mutation({
       query: (prompt) => ({
