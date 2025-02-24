8324cdf3ec9357db8b25e1d1f338e82d94054970
Saud_Khan <asaud3508@gmail.com>
2025-02-24 23:56:19 +0500

    ur

--- lib/redux/features/chatApiSlice.ts
+++ lib/redux/features/chatApiSlice.ts
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const chatApiSlice = createApi({
   reducerPath: "aiApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000", }),
+  baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000" }),
   endpoints: (builder) => ({
     prompt: builder.mutation({
       query: (prompt) => ({
-        url: "api/chat",
+        url: "/api/chat",
         method: "POST",
         body: { prompt: prompt },
         headers: {
