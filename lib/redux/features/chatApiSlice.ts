291df264489a04630c06b1ae60bc5f2b36e8169b
Saud_Khan <asaud3508@gmail.com>
2025-02-25 20:49:17 +0500

    url

--- lib/redux/features/chatApiSlice.ts
+++ lib/redux/features/chatApiSlice.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+const next_url = "https://ai-chat-beta-roan.vercel.app/";
 export const chatApiSlice = createApi({
   reducerPath: "aiApi",
-  baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: next_url || "http://localhost:3000" }),
   endpoints: (builder) => ({
     prompt: builder.mutation({
       query: (prompt) => ({
